17d6acb0d7fdf5ee964a8be7eb9648317fb37d76
Christopher Villamarín <alexpila.314@gmail.com>
2026-03-13 13:36:07 -0500

    feat: use the touchpad to go to the next slide

--- src/ui/backend_win32.c
+++ src/ui/backend_win32.c
@@ -4,24 +4,52 @@
 #include <cairo/cairo-win32.h>
 #include <stdio.h>
 
-// Variable global para mantener el estado del slider
 static Slider *g_slider = NULL;
 static int g_current_slide = 0;
 static int g_n_slides = 0;
+static int g_fullscreen = 0;
+
+// Estructura para recordar la posición de la ventana antes de ir a fullscreen
+static RECT g_prev_rect;
+static DWORD g_prev_style;
+
+static void toggle_fullscreen(HWND hwnd) {
+    if (!g_fullscreen) {
+        g_prev_style = GetWindowLong(hwnd, GWL_STYLE);
+        GetWindowRect(hwnd, &g_prev_rect);
+        
+        // Monitor actual
+        HMONITOR monitor = MonitorFromWindow(hwnd, MONITOR_DEFAULTTONEAREST);
+        MONITORINFO mi = { sizeof(mi) };
+        GetMonitorInfo(monitor, &mi);
+
+        SetWindowLong(hwnd, GWL_STYLE, g_prev_style & ~WS_OVERLAPPEDWINDOW);
+        SetWindowPos(hwnd, HWND_TOP, 
+                     mi.rcMonitor.left, mi.rcMonitor.top,
+                     mi.rcMonitor.right - mi.rcMonitor.left,
+                     mi.rcMonitor.bottom - mi.rcMonitor.top,
+                     SWP_NOOWNERZORDER | SWP_FRAMECHANGED);
+        g_fullscreen = 1;
+    } else {
+        SetWindowLong(hwnd, GWL_STYLE, g_prev_style);
+        SetWindowPos(hwnd, NULL, 
+                     g_prev_rect.left, g_prev_rect.top,
+                     g_prev_rect.right - g_prev_rect.left,
+                     g_prev_rect.bottom - g_prev_rect.top,
+                     SWP_NOZORDER | SWP_NOACTIVATE | SWP_FRAMECHANGED);
+        g_fullscreen = 0;
+    }
+}
 
 static void render_frame(HDC hdc, int w, int h) {
     if (!g_slider) return;
-
-    // Crear superficie Cairo sobre el DC de Windows
     cairo_surface_t *surface = cairo_win32_surface_create(hdc);
     cairo_t *cr = cairo_create(surface);
 
-    // Fondo (usando el tema actual)
-    const Theme *t = g_slider->theme; // Acceso directo al struct definido en internal.h
+    const Theme *t = g_slider->theme;
     cairo_set_source_rgb(cr, t->bg_r, t->bg_g, t->bg_b);
     cairo_paint(cr);
 
-    // Renderizar slide
     slider_render(g_slider, g_current_slide, cr, w, h);
 
     cairo_destroy(cr);
@@ -29,50 +57,60 @@ static void render_frame(HDC hdc, int w, int h) {
 }
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    int dirty = 0;
     switch (uMsg) {
     case WM_PAINT: {
         PAINTSTRUCT ps;
         HDC hdc = BeginPaint(hwnd, &ps);
-        
         RECT rect;
         GetClientRect(hwnd, &rect);
-        render_frame(hdc, rect.right, rect.bottom);
         
+        // Double buffering básico para evitar parpadeo
+        HDC memdc = CreateCompatibleDC(hdc);
+        HBITMAP membmp = CreateCompatibleBitmap(hdc, rect.right, rect.bottom);
+        SelectObject(memdc, membmp);
+        
+        render_frame(memdc, rect.right, rect.bottom);
+        BitBlt(hdc, 0, 0, rect.right, rect.bottom, memdc, 0, 0, SRCCOPY);
+        
+        DeleteObject(membmp);
+        DeleteDC(memdc);
         EndPaint(hwnd, &ps);
         return 0;
     }
-    case WM_KEYDOWN: {
-        int dirty = 0;
+    case WM_LBUTTONDOWN: // Click izquierdo -> Retroceder
+        if (g_current_slide > 0) { g_current_slide--; dirty = 1; }
+        break;
+    case WM_RBUTTONDOWN: // Click derecho -> Avanzar
+        if (g_current_slide < g_n_slides - 1) { g_current_slide++; dirty = 1; }
+        break;
+    case WM_KEYDOWN:
         switch (wParam) {
-        case VK_ESCAPE:
-        case 'Q':
-            PostQuitMessage(0);
-            return 0;
-        case VK_RIGHT:
-        case VK_SPACE:
-        case VK_RETURN:
+        case VK_ESCAPE: PostQuitMessage(0); return 0;
+        case 'Q': PostQuitMessage(0); return 0;
+        case VK_RIGHT: case VK_SPACE: case VK_RETURN: case VK_NEXT:
             if (g_current_slide < g_n_slides - 1) { g_current_slide++; dirty = 1; }
             break;
-        case VK_LEFT:
-        case VK_BACK:
+        case VK_LEFT: case VK_BACK: case VK_PRIOR:
             if (g_current_slide > 0) { g_current_slide--; dirty = 1; }
             break;
-        case VK_HOME:
-            g_current_slide = 0; dirty = 1;
-            break;
-        case VK_END:
-            g_current_slide = g_n_slides - 1; dirty = 1;
+        case VK_HOME: g_current_slide = 0; dirty = 1; break;
+        case VK_END: g_current_slide = g_n_slides - 1; dirty = 1; break;
+        case 'F': case VK_F11:
+            toggle_fullscreen(hwnd);
+            dirty = 1;
             break;
         }
-        if (dirty) {
-            InvalidateRect(hwnd, NULL, FALSE);
-        }
+        break;
+    case WM_SIZE:
+        InvalidateRect(hwnd, NULL, FALSE);
         return 0;
-    }
     case WM_DESTROY:
         PostQuitMessage(0);
         return 0;
     }
+
+    if (dirty) InvalidateRect(hwnd, NULL, FALSE);
     return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
 
